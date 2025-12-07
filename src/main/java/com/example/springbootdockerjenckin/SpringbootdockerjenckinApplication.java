183bdbd82edaf163c671515028cdb0665edd1d7f
Sudhanshu130989 <sudhanshucust@gmail.com>
2025-12-07 13:43:44 +0400

    first commit

---
+++ src/main/java/com/example/springbootdockerjenckin/SpringbootdockerjenckinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerjenckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerjenckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdockerjenckinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootdockerjenckin/SpringbootdockerjenckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerjenckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdockerjenckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
